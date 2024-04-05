92d0cf8f27473c0d009ef2025166950c2102b28d
윤 <dbstjd1576@gmail.com>
2024-04-05 15:00:43 +0900

    chapter1 미션제출

---
+++ Chapter1/윤/UMC_Week1_SB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UMC_Week1_SB
+//
+//  Created by 윤성 on 4/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
