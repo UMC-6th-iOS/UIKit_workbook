274945f4c729bcfb6aad87a811999d34e6876ba7
윤 <dbstjd1576@gmail.com>
2024-04-13 17:20:30 +0900

    Chapter2 미션 제출

---
+++ Chapter2/윤/UMC_Week2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  UMC_Week2
+//
+//  Created by 윤성 on 4/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var back: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        back.layer.cornerRadius = 10
+    }
+
+
+}
+
