27cf588cf1d95fcc897abc4105674a97e6be9875
문창재 <ckdwo23@gmail.com>
2024-06-28 16:26:35 +0900

    브랜치 테스트

--- Aehopark/Aehopark/ContentView.swift
+++ Aehopark/Aehopark/ContentView.swift
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("브랜치 테스트")
     }
 }
 
