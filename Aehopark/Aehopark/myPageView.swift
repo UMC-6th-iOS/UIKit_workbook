52be0a2cca9349e9527cc2c612ee193ca18913a6
윤 <dbstjd1576@gmail.com>
2024-06-28 16:27:22 +0900

    마이페이지

---
+++ Aehopark/Aehopark/myPageView.swift
@@ -0,0 +1,18 @@
+//
+//  myPageView.swift
+//  Aehopark
+//
+//  Created by 윤성 on 6/28/24.
+//
+
+import SwiftUI
+
+struct myPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    myPageView()
+}
